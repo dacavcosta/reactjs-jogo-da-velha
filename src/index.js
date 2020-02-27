c2aaad479603da42d29736910683957ad2940422
dontuta <dcavalcanticosta@gmail.com>
2020-02-26 23:41:02 -0300

    Destacando a jogada selecionada no histórico com negrito

--- src/index.js
+++ src/index.js
@@ -93,7 +93,9 @@ class Game extends React.Component {
       const desc = move ? `${move} - ${col},${row}` : 'Go to game start';
       return(
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button 
+            className={this.state.stepNumber === move ? 'selected-move' : ''}
+            onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
